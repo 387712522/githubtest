111d7251de7386b895add72a4e05733afcb4dcbb
387712522 <387712522@qq.com>
2022-06-30 23:51:22 +0800

    加注释

--- justfortest.c
+++ justfortest.c
@@ -2,7 +2,7 @@
  * @Author: baopucao
  * @Date: 2022-06-30 23:44:31
  * @LastEditors: baopucao
- * @LastEditTime: 2022-06-30 23:45:17
+ * @LastEditTime: 2022-06-30 23:49:40
  * @FilePath: /undefinedj:/Project/githubtest/githubtest/justfortest.c
  * @Description: file content
  */
@@ -10,5 +10,6 @@
 
 int main()
 {
+    //答应hello world
     printf("hello world");
 }
