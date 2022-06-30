91ebf8777ab5495b49753c82c44a7d3cb3351898
387712522 <387712522@qq.com>
2022-06-30 23:55:25 +0800

    用分支改动

--- justfortest.c
+++ justfortest.c
@@ -2,7 +2,7 @@
  * @Author: baopucao
  * @Date: 2022-06-30 23:44:31
  * @LastEditors: baopucao
- * @LastEditTime: 2022-06-30 23:49:40
+ * @LastEditTime: 2022-06-30 23:54:30
  * @FilePath: /undefinedj:/Project/githubtest/githubtest/justfortest.c
  * @Description: file content
  */
@@ -12,4 +12,5 @@ int main()
 {
     //答应hello world
     printf("hello world");
+    printf("just for test");
 }
