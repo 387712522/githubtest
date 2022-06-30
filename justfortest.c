e9b9d9f75132e2cf31fe98781e2f43c730092a3e
387712522 <387712522@qq.com>
2022-06-30 23:48:44 +0800

    first commit

---
+++ justfortest.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: baopucao
+ * @Date: 2022-06-30 23:44:31
+ * @LastEditors: baopucao
+ * @LastEditTime: 2022-06-30 23:45:17
+ * @FilePath: /undefinedj:/Project/githubtest/githubtest/justfortest.c
+ * @Description: file content
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+}
